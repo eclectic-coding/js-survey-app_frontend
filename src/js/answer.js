4318bfb49972ab88206775d8c9816f01af95c47d
eclecticcoding <chuck@eclecticsaddlebag.com>
2020-01-22 13:27:26 -0500

    # If applied, this commit will...
    # Why is this change needed?
    Fix results compilation bug.
    
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources

--- src/js/answer.js
+++ src/js/answer.js
@@ -4,9 +4,5 @@ class Answer {
     this.responded = responded
   }
 
-  clearMessage() {
-    console.log('Hello there')
-  }
-
 }
 
--- src/js/survey.js
+++ src/js/survey.js
@@ -10,7 +10,7 @@ class Survey {
 
   // Survey methods
   async deleteSurvey() {
-    await fetch(`http://localhost:3000/surveys/${ this.id }`, {
+    await fetch(`http://localhost:3000/surveys/${this.id}`, {
       method: 'DELETE'
     })
       .then(() => {
@@ -60,32 +60,27 @@ class Survey {
       })
   }
 
-  getResults() {
-    const fetchPromise = fetch('http://localhost:3000/answers')
+  getResults(myId) {
+    const fetchPromise = fetch(`http://localhost:3000/answers`)
     const resultsReport1 = document.getElementById('q1')
     const resultsReport2 = document.getElementById('q2')
     const resultsReport3 = document.getElementById('q3')
-    fetchPromise.then((response) => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        return Promise.reject(response)
-      }
-    }).then(questionResults => {
-      const myResults1 = questionResults.filter(a => a.surveys_id && a.responded === 'question1').length
-      resultsReport1.innerHTML += myResults1
-      const myResults2 = questionResults.filter(a => a.surveys_id && a.responded === 'question2').length
-      resultsReport2.innerHTML += myResults2
-      const myResults3 = questionResults.filter(a => a.surveys_id && a.responded === 'question3').length
-      resultsReport3.innerHTML += myResults3
-    })
+    fetchPromise.then(response => response.json())
+      .then(questionResults => {
+        const myResults1 = questionResults.filter(a => a.surveys_id === myId && a.responded === 'question1').length
+        resultsReport1.innerHTML += myResults1
+        const myResults2 = questionResults.filter(a => a.surveys_id === myId && a.responded === 'question2').length
+        resultsReport2.innerHTML += myResults2
+        const myResults3 = questionResults.filter(a => a.surveys_id === myId && a.responded === 'question3').length
+        resultsReport3.innerHTML += myResults3
+      })
   }
 
   surveyListHTML() {
     return `
     <div class="card__content">
       <div class="card__title">
-        <h3>${ this.title }</h3>
+        <h3>${this.title}</h3>
       </div>
     </div>
     <button class="card__btn">Take Survey</button>
@@ -93,19 +88,19 @@ class Survey {
   }
 
   surveyHTML() {
-    let ask1 = this.question1 ? `<input type="radio" name="answer_question" value="question1"> ${ this.question1 }` : ''
-    let ask2 = this.question2 ? `<input type="radio" name="answer_question" value="question2"> ${ this.question2 }` : ''
-    let ask3 = this.question3 ? `<input type="radio" name="answer_question" value="question3"> ${ this.question3 }` : ''
+    let ask1 = this.question1 ? `<input type="radio" name="answer_question" value="question1"> ${this.question1}` : ''
+    let ask2 = this.question2 ? `<input type="radio" name="answer_question" value="question2"> ${this.question2}` : ''
+    let ask3 = this.question3 ? `<input type="radio" name="answer_question" value="question3"> ${this.question3}` : ''
     return `
     <div class="card__content">
       <div class="card__title">
-        <h3>${ this.title }</h3>
+        <h3>${this.title}</h3>
       </div>
       <div class="card__summary">
         <form id="answer-form">
-          ${ ask1 }<br>
-          ${ ask2 }<br>
-          ${ ask3 }<br><br>
+          ${ask1}<br>
+          ${ask2}<br>
+          ${ask3}<br><br>
       </form>
       </div>
     </div>
@@ -119,9 +114,9 @@ class Survey {
     <div id="results-card">
       <h3>Results:</h3>
         <ul class="report-list">
-          <li>${ this.question1 }: <span id="q1"></span></li>
-          <li>${ this.question2 }: <span id="q2"></span></li>
-          <li>${ this.question3 }: <span id="q3"></span></li>
+          <li>${this.question1}: <span id="q1"></span></li>
+          <li>${this.question2}: <span id="q2"></span></li>
+          <li>${this.question3}: <span id="q3"></span></li>
         </ul>
      </div>
      <button class="card__btn done">Done</button>
@@ -132,15 +127,15 @@ class Survey {
     const resultsContainer = document.getElementById('survey-container')
     const resultsCard = document.createElement('div')
     resultsCard.classList.add('survey-card')
-    resultsCard.id = `results-${ this.id }`
+    resultsCard.id = `results-${this.id}`
     resultsCard.innerHTML += this.resultsHTML()
     resultsContainer.appendChild(resultsCard)
-    this.getResults()
+    this.getResults(this.id)
     resultsCard.addEventListener('click', e => {
       document.querySelector('survey-card')
       if (e.target.className.includes('done')) {
         document.getElementById('survey-container').removeChild(document.getElementById(this.id))
-        document.getElementById('survey-container').removeChild(document.getElementById(`results-${ this.id }`))
+        document.getElementById('survey-container').removeChild(document.getElementById(`results-${this.id}`))
         fetchSurveys()
       }
     })
